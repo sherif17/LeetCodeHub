98ef151e48ab2774e52da66ed19c14597c088e9e
Sherif Ahmed <43541909+sherif17@users.noreply.github.com>
2022-07-11 09:43:48 +0200

    Time: 7 ms (81.65%), Space: 13.8 MB (52.05%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int>dp(cost.size()+1);
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2; i<cost.size(); i++){
+            dp[i] = min(cost[i] + dp[i-1], cost[i] + dp[i-2]); // move to next or next to next step
+        }
+        
+        return min(dp[cost.size()-1], dp[cost.size()-2]);
+        
+        
+    }
+};
