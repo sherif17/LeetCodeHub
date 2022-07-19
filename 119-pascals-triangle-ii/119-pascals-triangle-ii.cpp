815717a210a1dc9547a0323bf9381cbfb12f72b9
Sherif Ahmed <43541909+sherif17@users.noreply.github.com>
2022-07-19 14:35:19 +0200

    Time: 0 ms (100.00%), Space: 6.7 MB (8.91%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        int n = rowIndex+1;
+        vector<vector<int>>ans;
+         if( n == 1)
+        {
+			// edge case just push {1} to ans vec tor
+            ans.push_back({1});
+        }
+        if(n == 2)
+        {
+			// edge case just push {1}  and {1,1}  ans vec tor
+            ans.push_back({1});
+            ans.push_back({1,1});
+        }
+        if(n>=3){
+          // repeat these prior steps
+            ans.push_back({1});
+            ans.push_back({1,1});
+           int level = 2;
+            
+            for(int i = 1; i<n-1; ++i)
+            {
+                vector<int> v;
+                for(int j = 0; j<= level; ++j)
+                {
+					// edges case first element of current row has to be first element of prev row.
+                    if(j == 0)
+                        v.push_back(ans[i][0]);
+                    else if(j == level)
+                    {
+							// edges case last element of current row has to be last element of prev row.
+                        v.push_back(ans[i][level-1]);
+                    }
+                    else
+                    {
+                        v.push_back(ans[i][j-1] + ans[i][j]);
+                    }    
+                }
+                ++level;
+                ans.push_back(v);
+            } 
+        }
+        return ans[rowIndex];
+        
+    }
+};
